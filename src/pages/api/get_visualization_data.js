b2ea72613ef2c649717d34bffec1be78018e5331
AlexBabkf <bab_alt@hotmail.com>
2023-09-01 01:09:51 +0200

    changed fetch for deployment

--- src/pages/api/get_visualization_data.js
+++ src/pages/api/get_visualization_data.js
@@ -1,6 +1,8 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("http://127.0.0.1:5000/");
+    // const response = await fetch("http://127.0.0.1:5000/"); // development
+    const response = await fetch("https://pycovid.vercel.app/api"); // deployment
+
     const visualizationData = await response.json();
     res.status(200).json(visualizationData);
   } catch (error) {
