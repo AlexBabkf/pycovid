72b2abfee407788fe6d34f2a57472d1deb7f69c0
AlexBabkf <bab_alt@hotmail.com>
2023-09-01 01:18:24 +0200

    changed src for deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ const HomePage = () => {
 
       <iframe
         // src={`http://127.0.0.1:5000/${selectedVisualization}`} // development
-        src={`https://pycovid.vercel.app/${selectedVisualization}`} // deployment
+        src={`https://pycovid.vercel.app/api/${selectedVisualization}`} // deployment
         style={{ width: "100%", height: "500px", border: "none" }}
         title={selectedVisualization}
       ></iframe>
