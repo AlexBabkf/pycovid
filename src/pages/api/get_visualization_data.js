a6b032ed90d105f22601c17e7d17e2e2de329db1
Georgios Alexandros Christodoulou <bab_alt@hotmail.com>
2023-09-01 13:33:40 +0200

    again vercel

--- src/pages/api/get_visualization_data.js
+++ src/pages/api/get_visualization_data.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
-    // const response = await fetch("http://127.0.0.1:5000/"); // development
-    const response = await fetch("https://pycovid.vercel.app/api"); // deployment
+    const response = await fetch("http://127.0.0.1:5000/"); // development
+    // const response = await fetch("https://pycovid.vercel.app/api"); // deployment
 
     const visualizationData = await response.json();
     res.status(200).json(visualizationData);
